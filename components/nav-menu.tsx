f90e167086370e88541dc356af78b6d7d29cdd13
Md Ahnaf Abir <ahnafabir313@gmail.com>
2025-11-26 16:14:16 +0600

    testimonials menu removed

--- components/nav-menu.tsx
+++ components/nav-menu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Home, MessageCircleMore, Briefcase, Mail, Users } from "lucide-react";
+import { Home, Briefcase, Mail, Users } from "lucide-react";
 import { NavBar } from "@/components/tubelight-navbar";
 import { Suspense } from "react";
 
@@ -8,7 +8,6 @@ export function NavigationMenu() {
   const navItems = [
     { name: "Home", url: "/#home", icon: Home },
     { name: "Services", url: "/#services", icon: Briefcase },
-    { name: "Testimonials", url: "/#testimonials", icon: MessageCircleMore },
     { name: "Contact", url: "/#contact", icon: Mail },
     { name: "About Us", url: "/about", icon: Users },
   ];
